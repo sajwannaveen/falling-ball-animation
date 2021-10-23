49fdb7a26929cf784ce93f42344df3302cec14a7
Rahul Kumar Jha <67184791+RahulJha393@users.noreply.github.com>
2021-10-24 02:14:33 +0530

    Added new function
    new controller are being added in the programm

--- falling ball animation.py
+++ falling ball animation.py
@@ -23,6 +23,33 @@ def ball(posX,radius,color,maxi):
             cir(color, tuple(pos),radius)
             if(pos[1]>=maxi):
                  break
+   # Main loop
+    while running:
+        # Look at every event in the queue
+        for event in pygame.event.get():
+            # Did the user hit a key?
+            if event.type == KEYDOWN:
+                # Was it the Escape key? If so, stop the loop.
+                if event.key == K_ESCAPE:
+                    running = False
+
+            # Did the user click the window close button? If so, stop the loop.
+            elif event.type == QUIT:
+                running = False
+
+            # Add a new enemy?
+            elif event.type == ADDENEMY:
+                # Create the new enemy and add it to sprite groups
+                new_enemy = Enemy()
+                enemies.add(new_enemy)
+                all_sprites.add(new_enemy)
+
+        # Get the set of keys pressed and check for user input
+        pressed_keys = pygame.key.get_pressed()
+        player.update(pressed_keys)
+
+    # Update enemy position
+    enemies.update()
             
         while(1==1):
             for event in pygame.event.get():
